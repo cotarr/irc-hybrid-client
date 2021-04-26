da9534c705582de80b7b55369f855b803cdcbdca
cotarr <cotarr@yahoo.com>
2021-04-26 15:35:08 -0500

    Extract write to IRC socket as irc-client-write.js module

--- server/irc/irc-client.js
+++ server/irc/irc-client.js
@@ -33,7 +33,8 @@
   const isValidUTF8 = require('utf-8-validate');
   const ircLog = require('./irc-client-log');
   const ircMessageCache = require('./irc-client-cache');
-  const vars = require('./irc-client-vars');
+  const ircWrite = require('./irc-client-write');
+  var vars = require('./irc-client-vars');
 
   var nodeEnv = process.env.NODE_ENV || 'development';
 
@@ -101,92 +102,6 @@
     global.sendToBrowser('UPDATE\r\n');
   }
 
-  // ----------------------------------------------------
-  // Write data to IRC server socket (Internal function)
-  // Includes check of socket status
-  //
-  // Expect Buffer or utf8 encoded string.
-  //
-  // Send utf8 string to browser
-  // Send Buffer encoded utf8 to IRC server socket
-  // ----------------------------------------------------
-  const _writeSocket = function (socket, message) {
-    if (message.length === 0) return;
-    if ((socket) && (socket.writable)) {
-      //
-      // First, filter passwords, then send to browser and write log file
-      // The filter only applies to log rile and browser.
-      // Messages to IRC server are not filtered.
-      //
-      let filterWords = [
-        'OPER',
-        'PASS',
-        'NICKSERV',
-        'NS',
-        'CHANSERV',
-        'CS'
-      ];
-      let filterCommand = message.toString().split(' ')[0].toUpperCase();
-      let filtered = message.toString();
-      if (filterWords.indexOf(filterCommand) >= 0) {
-        if (!vars.ircState.ircTLSEnabled) {
-          console.log('WARNING: possible password without TLC encryption to IRC server');
-        }
-        filtered = filterCommand + ' ***********';
-      }
-      if (message.split(' ')[0].toUpperCase() === 'JOIN') {
-        if ((message.split(' ').length > 2) && (message.split(' ')[2].length > 0)) {
-          filtered = 'JOIN ' + message.split(' ')[1] + ' ********';
-        }
-      }
-
-      // Looking for: 'PRIVMSG NickServ :IDENTIFY <passowrd>'
-      if (message.split(' ')[0].toUpperCase() === 'PRIVMSG') {
-        if ((message.split(' ').length > 3) &&
-          (message.split(' ')[1].toLowerCase() === 'nickserv') &&
-          (message.split(' ')[2].toUpperCase() === ':IDENTIFY')) {
-          filtered = 'PRIVMSG ' +
-            message.split(' ')[1] + ' ' + message.split(' ')[2] + ' ********';
-        }
-      }
-      global.sendToBrowser(vars.commandMsgPrefix + filtered + '\n');
-
-      //
-      // Second validate the string before writing to IRC server socket
-      //
-      let out = null;
-      if (typeof message === 'string') {
-        out = Buffer.from(message + '\r\n', 'utf8');
-      }
-      if (Buffer.isBuffer(message)) {
-        out = Buffer.concat([out, Buffer.from('\r\n', 'utf8')]);
-      }
-      if (!isValidUTF8(out)) {
-        out = null;
-        console.log('_writeSocket failed UTF-8 validation');
-      }
-      // RFC 2812 does not allow zero character
-      if (out.includes(0)) {
-        out = null;
-        console.log('_writeSocket failed zero byte validation');
-      }
-      // 512 btye maximum size from RFC 2812 Section 2.3 Messages
-      // It is assumed here this means 8 bit types not multi-byte characters
-      if (out.length > 512) {
-        out = null;
-        console.log('_writeSocket send buffer exceeds 512 character limit.');
-      }
-      //
-      // Third, send into socket to IRC server
-      //
-      if (out) {
-        socket.write(Buffer.concat([out, Buffer.from('\r\n', 'utf8')]));
-      }
-    } else {
-      // TODO should this disconnect?
-      console.log('_writeSocket server socket not writable');
-    }
-  }; // _writeSocket
 
   // -------------------------------------------
   //  On Ready Event Handler (Internal function)
@@ -197,13 +112,15 @@
       // check state, if error occurred this will be false.
       if (vars.ircState.ircConnecting) {
         if ((ircServerPassword) && (ircServerPassword.length > 0)) {
-          _writeSocket(socket, 'PASS ' + ircServerPassword);
+          ircWrite.writeSocket(socket, 'PASS ' + ircServerPassword);
         }
-        _writeSocket(socket, 'NICK ' + vars.ircState.nickName);
+        ircWrite.writeSocket(socket, 'NICK ' + vars.ircState.nickName);
         // Note: mode 8 = i not working on ngirc ?
-        _writeSocket(socket, 'USER ' + vars.ircState.userName + ' 0 * :' + vars.ircState.realName);
+        ircWrite.writeSocket(socket, 'USER ' + vars.ircState.userName +
+          ' 0 * :' + vars.ircState.realName);
         if (vars.ircState.userMode.length > 0) {
-          _writeSocket(socket, 'MODE ' + vars.ircState.nickName + ' ' + vars.ircState.userMode);
+          ircWrite.writeSocket(socket, 'MODE ' + vars.ircState.nickName +
+            ' ' + vars.ircState.userMode);
         }
         vars.ircState.ircConnecting = false;
         vars.ircState.ircConnected = true;
@@ -498,7 +415,7 @@
       if (checkCtcpNotFlood()) {
         global.sendToBrowser(outgoingBackToSelf + '\r\n');
         ircMessageCache.addMessage(Buffer.from(outgoingBackToSelf));
-        _writeSocket(socket, ircMessage);
+        ircWrite.writeSocket(socket, ircMessage);
       }
     }
     const ctcpDelim = 1;
@@ -786,7 +703,7 @@
             setTimeout(function() {
               if ((vars.ircState.ircConnected) &&
                 (vars.ircState.nickName === nsIdentifyNick)) {
-                _writeSocket(socket, nsIdentifyCommand);
+                ircWrite.writeSocket(socket, nsIdentifyCommand);
               }
             }.bind(this), 1500);
           }
@@ -1554,7 +1471,7 @@
             res.json({error: true, message: parseResult.message});
           } else {
             // Send browser message on to web server
-            _writeSocket(ircSocket, message);
+            ircWrite.writeSocket(ircSocket, message);
             res.json({error: false});
           }
         }
