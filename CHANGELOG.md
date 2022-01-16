2dd46ff30ce350a8fb3899b563e44dccddf898e2
Dave Bolenbaugh <cotarr@yahoo.com>
2022-01-16 15:08:26 -0600

    Update helmet v5.0.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -12,6 +12,7 @@ and this project adheres to
 ### Changed
 
 * web-server.js - Remove package bodyparser, instead use express.json() and express.urlencoded()
+* web-server.js - Upgrade helmet from v4 to v5.0.1, changes to match new version
 * Upgrade packages express@4.17.2, express-session@1.17.2, node-fetch@2.6.7, utf-8-validate@5.0.8, ws@8.4.2
 
 ## [v0.1.34](https://github.com/cotarr/irc-hybrid-client/releases/tag/v0.1.34) 2022-01-01
--- server/web-server.js
+++ server/web-server.js
@@ -140,14 +140,15 @@ if (nodeEnv === 'development') {
 // ------------------------------
 // Content Security Policy (CSP)
 // ------------------------------
-// -- Helmet CSP defaults v4.6.0 --
+// -- Helmet CSP defaults v5.0.1 --
+//
 // default-src 'self';
 // base-uri 'self';
 // block-all-mixed-content;
 // font-src 'self' https: data:;
+// form-action 'self';
 // frame-ancestors 'self';
-// img-src 'self' data:;
-// object-src 'none';
+// img-src 'self' data:;object-src 'none';
 // script-src 'self';
 // script-src-attr 'none';
 // style-src 'self' https: 'unsafe-inline';
@@ -186,16 +187,22 @@ if (credentials.tls) {
 // ----------------------------------------
 // HTTP Security Headers
 // ----------------------------------------
-// -- Helmet Default headers v4.6.0 --
-// X-DNS-Prefetch-Control off
-// Expect-CT max-age=0
-// X-Frame-Options SAMEORIGIN
-// Strict-Transport-Security max-age=15552000; includeSubDomains
-// X-Download-Options noopen
-// X-Content-Type-Options nosniff
-// X-Permitted-Cross-Domain-Policies none
-// Referrer-Policy no-referrer
-// X-XSS-Protection 0
+// -- Helmet Default headers Helmet v5.0.1 --
+//
+// Cross-Origin-Embedder-Policy: require-corp
+// Cross-Origin-Opener-Policy: same-origin
+// Cross-Origin-Resource-Policy: same-origin
+// Expect-CT: max-age=0
+// Origin-Agent-Cluster: ?1
+// Referrer-Policy: no-referrer
+// Strict-Transport-Security: max-age=15552000; includeSubDomains
+// X-Content-Type-Options: nosniff
+// X-DNS-Prefetch-Control: off
+// X-Download-Options: noopen
+// X-Frame-Options: SAMEORIGIN
+// X-Permitted-Cross-Domain-Policies: none
+// X-XSS-Protection: 0
+// X-Powered-By: ( Removed by helmet)
 // ----------------------------------------
 
 // ----------------------------------------
