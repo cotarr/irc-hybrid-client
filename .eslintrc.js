d887cd462c8983c0eda7d57bd34866876f0ec18f
Dave Bolenbaugh <cotarr@yahoo.com>
2022-08-01 10:14:31 -0500

    Upgrade eslint to Version 8

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     'standard'
   ],
   parserOptions: {
-    ecmaVersion: 12
+    ecmaVersion: 'latest'
   },
   rules: {
     // Require parenthesis around arrow function argument
@@ -17,7 +17,8 @@ module.exports = {
     'comma-dangle': ['error', 'never'],
     'linebreak-style': ['error', 'unix'],
     'max-len': ['error', { code: 100, tabWidth: 2, ignoreUrls: true }],
-    semi: ['error', 'always']
+    semi: ['error', 'always'],
+    'object-shorthand': 'off'
   },
   overrides: [
     {
