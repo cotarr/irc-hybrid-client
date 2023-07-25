3109c69518854c95dc60a7d1e539d8f02ae869a3
Dave Bolenbaugh <cotarr@yahoo.com>
2023-07-25 09:42:47 -0500

    v0.2.50 Removed previous v0.2.49 change, logging of server list editor

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,14 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to
 [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [v0.2.50](https://github.com/cotarr/irc-hybrid-client/releases/tag/v0.2.50) 2023-07-25
+
+## Removed
+
+Backed out changes from v0.2.49. The server list editor page can become 
+unresponsive after posting a server edit to the API. This was a 
+feature to add edit history to the log files.
+
 ## [v0.2.49](https://github.com/cotarr/irc-hybrid-client/releases/tag/v0.2.49) 2023-07-20
 
 ### Added
--- server/irc/irc-serverlist-editor.mjs
+++ server/irc/irc-serverlist-editor.mjs
@@ -806,6 +806,7 @@ const toggleServerDisabled = function (req, chainObject) {
  * @param {Object} chainObject - Wrapper object used to pass common date through promise chain
  * @returns {Promise} Resolve to Object (chainObject) or reject error
  */
+// eslint-disable-next-line no-unused-vars
 const logger = (req, chainObject) => {
   return new Promise((resolve, reject) => {
     let method = null;
@@ -905,7 +906,7 @@ const create = function (req, res, next) {
     .then((chainObject) => deserializeElements(req, chainObject))
     .then((chainObject) => appendArrayElement(chainObject))
     .then((chainObject) => writeServersFile(chainObject))
-    .then((chainObject) => logger(req, chainObject))
+    // .then((chainObject) => logger(req, chainObject))
     .then((chainObject) => returnStatus(req, res, chainObject))
     .catch((err) => next(err));
 };
@@ -923,7 +924,7 @@ const update = function (req, res, next) {
     .then((chainObject) => deserializeElements(req, chainObject))
     .then((chainObject) => replaceArrayElement(req, chainObject))
     .then((chainObject) => writeServersFile(chainObject))
-    .then((chainObject) => logger(req, chainObject))
+    // .then((chainObject) => logger(req, chainObject))
     .then((chainObject) => returnStatus(req, res, chainObject))
     .catch((err) => handlePromiseErrors(next, err));
 };
@@ -941,7 +942,7 @@ const copy = function (req, res, next) {
     .then((chainObject) => copyExistingServer(req, chainObject))
     .then((chainObject) => appendArrayElement(chainObject))
     .then((chainObject) => writeServersFile(chainObject))
-    .then((chainObject) => logger(req, chainObject))
+    // .then((chainObject) => logger(req, chainObject))
     .then((chainObject) => returnStatus(req, res, chainObject))
     .catch((err) => next(err));
 };
@@ -955,7 +956,7 @@ const destroy = function (req, res, next) {
     .then((chainObject) => requireNotLock(chainObject))
     .then((chainObject) => readServersFile(chainObject))
     .then((chainObject) => addMissingProperties(chainObject))
-    .then((chainObject) => logger(req, chainObject))
+    // .then((chainObject) => logger(req, chainObject))
     .then((chainObject) => deleteArrayElement(req, chainObject))
     .then((chainObject) => writeServersFile(chainObject))
     .then((chainObject) => returnStatus(req, res, chainObject))
